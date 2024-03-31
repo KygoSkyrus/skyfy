dc4fae435e8dac94f684ac5404e265565745bf56
KygoSkyrus <guptadheeraj371@gmail.com>
2024-03-31 22:41:14 +0530

    initial

---
+++ app/index.js
@@ -0,0 +1,15 @@
+// import { View, Text } from "react-native-web";
+import { View, Text, FlatList } from "react-native";
+
+const Home = () => {
+
+    const test = "hell"
+    return (
+        <View>
+            <Text style={{backgroundColor:"gray", color:"yellow", borderRadius:8, padding:30}}>HEyfgd here</Text>
+        </View >
+    )
+
+}
+
+export default Home;
